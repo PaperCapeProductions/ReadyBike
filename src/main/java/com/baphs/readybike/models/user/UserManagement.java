fb05d1ee89875d2739fed432b0e606027b007da7
Andres Pineda <andres@pineda.ws>
2016-03-20 16:19:39 -0400

    Removed unnecessary code from UserManagement

--- src/main/java/com/baphs/readybike/models/user/UserManagement.java
+++ src/main/java/com/baphs/readybike/models/user/UserManagement.java
@@ -1,42 +1,19 @@
 package com.baphs.readybike.models.user;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.baphs.readybike.utils.HBMSessionFactory;
-
 public class UserManagement {
 	
-	private static final Logger logger = LoggerFactory.getLogger(UserManagement.class);
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserManagement.class);
 	
 	public UserManagement() {
 		
 	}
 	
-	public Integer addUser(final String firstName, final String lastName) {
-		Session session = HBMSessionFactory.getSessionFactory().openSession();
-		Transaction tx = null;
-		Integer userID = null;
+	public void addUser(final String firstName, final String lastName) {
 		
-		try{
-			tx = session.beginTransaction();
-			User user = new User(firstName, lastName);
-			userID = (Integer) session.save(user);
-			tx.commit();
-		} catch (HibernateException e) {
-			if (tx != null) {
-				tx.rollback();
-			}
-			if (logger.isDebugEnabled()) {
-				logger.debug(getClass().getName(), e.getMessage());
-			}
-		} finally {
-			session.close();
-		}
-		
-		return userID;
 	}
 }
